9bad7d59123ec0f3a8655cea4bfd9654d85738df
israelgms <71859464+israelgms@users.noreply.github.com>
2021-12-16 22:17:25 -0300

    adicionando users.route

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import express, {Request, Response, NextFunction} from 'express'
+import usersRoute from './routes/users.route';
 
 const app = express();
 
+app.use(usersRoute);
+
 app.get('/status', (req: Request, res: Response , next: NextFunction) => {
-    res.status(200).send({ welcome: 'Bem vindo' });
+    res.status(200).send({ status: 'Sucesso!' });
 })
 
 app.listen(3000, () => {
